877bdcc3e01d6a49fccd04ebcba77eb8e2e3b125
Conquistador9 <varuzhan1014@mail.ru>
2026-02-11 13:35:20 +0300

    Commit7

---
+++ Assets/Scripts/PlaySong.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySong : MonoBehaviour
+{
+    private AudioSource _audio;
+
+    private void Start()
+    {
+        _audio = GetComponent<AudioSource>();
+        StartCoroutine(SongActiveTime());
+    }
+
+    private IEnumerator SongActiveTime()
+    {
+        yield return new WaitForSeconds(10);
+        _audio.enabled = true;
+    }
+}
